7aa46b76e85cb9c784fbc38c7558914c77adfb65
Josh Ke <joshua@Joshs-MacBook-Pro.local>
2023-04-03 21:55:31 -0400

    implement mysql try 1

--- test_streamlit.py
+++ test_streamlit.py
@@ -93,3 +93,30 @@ active_physicians = active_physicians.dropna(subset=['lat', 'lon'])
 # active_physicians['lat']
 
 st.map(active_physicians)
+
+# streamlit_app.py
+
+import streamlit as st
+import mysql.connector
+
+# Initialize connection.
+# Uses st.cache_resource to only run once.
+@st.cache_resource
+def init_connection():
+    return mysql.connector.connect(**st.secrets["mysql"])
+
+conn = init_connection()
+
+# Perform query.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * from mytable;")
+
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
